68bee53b054a6b4bf4fe3046c0d0b3141fdf1676
McQueen <soslovemiss@dingtalk.com>
2021-05-04 17:06:58 +0800

    邮件的密码

--- yeb-server/src/main/java/com/kou/server/controller/AdminInfoController.java
+++ yeb-server/src/main/java/com/kou/server/controller/AdminInfoController.java
@@ -9,10 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
@@ -48,9 +45,10 @@ public class AdminInfoController {
     }
 
     @ApiOperation(value = "更换头像")
-    @PostMapping("/admin/userface")
+    @PostMapping("/admin/userFace")
     public RespBean updateAdminUserFace(MultipartFile file,Integer id,Authentication authentication){
         String[] filePath = FastDfsUtils.upload(file);
+        //关键点，将我们的图片放在了FastDfs中了
         String url=FastDfsUtils.getTrackerUrl()+filePath[0]+"/"+filePath[1];
         return adminService.updateAdminUserFace(url,id,authentication);
     }
